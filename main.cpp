d310f61299d330323105381cfaed829d62b455e3
RimYT <rimsolovev@gmail.com>
2026-04-21 20:26:02 +0300

    Added frc::isGreater() func

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 Fraction f1 = Fraction(5, -10);
 Fraction f2 = Fraction(1, 8);
 Fraction f3 = 10;
-Fraction f4 = Fraction(3, 2);
+Fraction f4 = Fraction(10, 15);
+Fraction f5 = Fraction(2, 3);
 
 int main() {
     AUTO_SIMPLIFY = true;
-    floatToFraction(1.8).getRemainder().print();
+    cout << isGreater(f5, f4);
     return 0;
 }
--- src/fractions.cpp
+++ src/fractions.cpp
@@ -106,6 +106,19 @@ namespace frc {
 
         return false;
     }
+    bool isGreater(const Fraction& f1, const Fraction& f2) {
+        long f1_num = f1.getNumerator();
+        long f1_den = f1.getDenominator();
+
+        long f2_num = f2.getNumerator();
+        long f2_den = f2.getDenominator();
+
+        if(f1_num * f2_den > f2_num * f1_den) {
+            return true;
+        }
+
+        return false;
+    }
 
     // -------- arithmetic operations --------
     Fraction sum(const Fraction& f1, const Fraction& f2) {
